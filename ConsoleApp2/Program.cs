2c5b2b9d50dee2c2b463ca0c89aa879c7355220b
Semih <Semih@DESKTOP-OB76QRL>
2021-10-03 18:04:36 +0300

    unf
    unf

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -12,10 +12,146 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            İnstagramKayit();
+            FindTheUnf();
+        }
+        public static IReadOnlyCollection<IWebElement> TakipEttiklerim(Bilgiler bilgi, IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl($"https://www.instagram.com/" + bilgi.yonlenicektag);
+            Thread.Sleep(7000);
+            IWebElement takipEdilenButonu= driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(3) > a"));
+            takipEdilenButonu.Click();
+            Thread.Sleep(2500);
+            string jsCommand = "" +
+                "sayfa = document.querySelector('.isgrP');" +
+                "sayfa.scrollTo(0,sayfa.scrollHeight);" +
+                "var sayfaSonu = sayfa.scrollHeight;" +
+                "return sayfaSonu;";
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            var sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
+
+            while (true)
+            {
+                var son = sayfaSonu;
+                Thread.Sleep(750);
+                sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
+                if (son == sayfaSonu)
+                    break;
+            }
+
+            IReadOnlyCollection<IWebElement> follwers = driver.FindElements(By.CssSelector(".FPmhX.notranslate._0imsa"));
+         
+            return follwers;
+
         }
 
+        public static void FindTheUnf()
+        {
+            var options = new OpenQA.Selenium.Chrome.ChromeOptions();
+            options.BinaryLocation = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
+            using (IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver(options))
+            {
 
+                var UnfList=new List<string>();
+                var followerList = new List<string>();
+                var followList = new List<string>();
+
+                var bilgi = Login(driver);
+                var follwersWebElement = Takipçiler(bilgi, driver);
+                Thread.Sleep(7000);
+                foreach (IWebElement item in follwersWebElement)
+                {
+                    followerList.Add(item.Text);
+                }
+
+                var followWebElement=TakipEttiklerim(bilgi,driver);
+                foreach (IWebElement item in followWebElement)
+                {
+                    followList.Add(item.Text);
+                }
+
+                foreach (var item in followList)
+                {
+                    if(followerList.Where(x=>x == item).Count() <= 0)
+                    {
+                        UnfList.Add(item);
+                        Console.WriteLine(item);
+                    }
+                }
+
+                
+            }
+            Console.ReadKey();
+        }
+
+        public static IReadOnlyCollection<IWebElement> Takipçiler(Bilgiler bilgi, IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl($"https://www.instagram.com/" + bilgi.yonlenicektag);
+            Thread.Sleep(7000);
+            IWebElement followerLink = driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(2) > a"));
+            followerLink.Click();
+            Thread.Sleep(5000);
+            string jsCommand = "" +
+                "sayfa = document.querySelector('.isgrP');" +
+                "sayfa.scrollTo(0,sayfa.scrollHeight);" +
+                "var sayfaSonu = sayfa.scrollHeight;" +
+                "return sayfaSonu;";
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            var sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
+
+            while (true)
+            {
+                var son = sayfaSonu;
+                Thread.Sleep(750);
+                sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
+                if (son == sayfaSonu)
+                    break;
+            }
+
+            IReadOnlyCollection<IWebElement> follwers = driver.FindElements(By.CssSelector(".FPmhX.notranslate._0imsa"));
+
+            return follwers;
+        }
+
+        public static Bilgiler Login(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl("https://www.instagram.com");
+            Thread.Sleep(2000);
+            IWebElement userName = driver.FindElement(By.Name("username"));
+            IWebElement password = driver.FindElement(By.Name("password"));
+            IWebElement girisbutonu = driver.FindElement(By.CssSelector(".sqdOP.L3NKy.y3zKF"));
+            Bilgiler bilgi = new Bilgiler();
+            bilgi.kullaniciAdi = "";
+            bilgi.sifre = "";
+            bilgi.yonlenicektag = "";
+            userName.SendKeys(bilgi.kullaniciAdi);
+            password.SendKeys(bilgi.sifre);
+            girisbutonu.Click();
+            Thread.Sleep(7000);
+
+            return bilgi;
+        }
+
+        public static void NameFollow(IReadOnlyCollection<IWebElement> follwers, IWebDriver driver)
+        {
+            foreach (IWebElement follower in follwers)
+            {
+                try
+                {
+                    driver.Navigate().GoToUrl($"https://www.instagram.com/" + follower.Text);
+                    var Followbtn = driver.FindElement(By.XPath("//button[. = 'Takip Et']"));
+                    Followbtn.Click();
+                    Thread.Sleep(2500);
+                }
+                catch (Exception)
+                {
+
+                    Thread.Sleep(2500);
+                }
+
+            }
+        }
 
         public static void Follow()
         {
@@ -23,60 +159,10 @@ namespace ConsoleApp2
             options.BinaryLocation = @"C:\Program Files\Google\Chrome\Application\chrome.exe";
             using (IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver(options))
             {
-                driver.Navigate().GoToUrl("https://www.instagram.com");
-                Thread.Sleep(2000);
-                IWebElement userName = driver.FindElement(By.Name("username"));
-                IWebElement password = driver.FindElement(By.Name("password"));
-                IWebElement girisbutonu = driver.FindElement(By.CssSelector(".sqdOP.L3NKy.y3zKF"));
-                Bilgiler bilgi = new Bilgiler();
-                bilgi.kullaniciAdi = "";
-                bilgi.sifre = "";
-                bilgi.yonlenicektag = "";
-                userName.SendKeys(bilgi.kullaniciAdi);
-                password.SendKeys(bilgi.sifre);
-                girisbutonu.Click();
-                Thread.Sleep(7000);
-                driver.Navigate().GoToUrl($"https://www.instagram.com/" + bilgi.yonlenicektag);
-                Thread.Sleep(7000);
-                IWebElement followerLink = driver.FindElement(By.CssSelector("#react-root > section > main > div > header > section > ul > li:nth-child(2) > a"));
-                followerLink.Click();
-                Thread.Sleep(2500);
-                string jsCommand = "" +
-                    "sayfa = document.querySelector('.isgrP');" +
-                    "sayfa.scrollTo(0,sayfa.scrollHeight);" +
-                    "var sayfaSonu = sayfa.scrollHeight;" +
-                    "return sayfaSonu;";
 
-                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-                var sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
-
-                while (true)
-                {
-                    var son = sayfaSonu;
-                    Thread.Sleep(750);
-                    break;
-                    sayfaSonu = Convert.ToInt32(js.ExecuteScript(jsCommand));
-                    if (son == sayfaSonu)
-                        break;
-                }
-
-                IReadOnlyCollection<IWebElement> follwers = driver.FindElements(By.CssSelector(".FPmhX.notranslate._0imsa"));
-                foreach (IWebElement follower in follwers)
-                {
-                    try
-                    {
-                        driver.Navigate().GoToUrl($"https://www.instagram.com/" + follower.Text);
-                        var Followbtn = driver.FindElement(By.XPath("//button[. = 'Takip Et']"));
-                        Followbtn.Click();
-                        Thread.Sleep(2500);
-                    }
-                    catch (Exception)
-                    {
-
-                        Thread.Sleep(2500);
-                    }
-
-                }
+                var bilgi = Login(driver);
+                var follwers = Takipçiler(bilgi, driver);
+                NameFollow(follwers, driver);
             }
         }
 
@@ -159,10 +245,10 @@ namespace ConsoleApp2
                 IWebElement password = driver.FindElement(By.Name("password"));
                 IWebElement Kayıtbutton = driver.FindElement(By.XPath("//button[. = 'Kaydol']"));
                 User user = new User();
-                user.adsoyad = "ckljyoh1";
+                user.adsoyad = "";
                 user.eposta = tempmail;
-                user.kullaniciadi = "ckljyoh11";
-                user.sifre = "semih3189";
+                user.kullaniciadi = "";
+                user.sifre = "";
                 email.SendKeys(user.eposta);
                 fullname.SendKeys(user.adsoyad);
                 username.SendKeys(user.kullaniciadi);
@@ -196,7 +282,7 @@ namespace ConsoleApp2
                     try
                     {
                         sayaç++;
-                        if(sayaç == 3000)
+                        if (sayaç == 3000)
                         {
                             Thread.Sleep(2500);
                             driver.SwitchTo().Window(driver.WindowHandles.Last());
@@ -206,7 +292,7 @@ namespace ConsoleApp2
                             driver.SwitchTo().Window(driver.WindowHandles.First());
                             sayaç = 0;
                         }
-                         codelink = driver.FindElement(By.CssSelector("#email_content > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(2)")).GetAttribute("innerHTML");
+                        codelink = driver.FindElement(By.CssSelector("#email_content > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td > table > tbody > tr:nth-child(2) > td:nth-child(2) > table > tbody > tr:nth-child(2) > td:nth-child(2)")).GetAttribute("innerHTML");
 
                         break;
                     }
@@ -220,7 +306,7 @@ namespace ConsoleApp2
 
                 try
                 {
-               
+
                     IWebElement emailconfirmationcode = driver.FindElement(By.Name("email_confirmation_code"));
                     emailconfirmationcode.SendKeys(codelink);
 
@@ -229,17 +315,17 @@ namespace ConsoleApp2
 
 
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
 
                     Console.WriteLine(ex.Message);
                 }
-               
+
                 Thread.Sleep(20000);
 
 
 
-   
+
             }
 
         }
